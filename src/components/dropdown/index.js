84f011e9bfbe1ba7cedc89d69dcfa99da65d873e
Tanmay Bhatt <tanmaybhatt.iet@gmail.com>
2017-12-21 15:05:10 +0530

    Fixed text not changing issue on prop change

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -147,9 +147,7 @@ export default class Dropdown extends PureComponent {
   }
 
   componentWillReceiveProps({ value }) {
-    if (value !== this.props.value) {
       this.setState({ value });
-    }
   }
 
   componentDidMount() {
